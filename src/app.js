5c00a9e071c1b5d1be384d61365f801f981f7938
Azizbek <Uygurskiy93@gmail.com>
2023-03-08 16:02:55 +0600

    add section

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    Infinity: true
+});
+
